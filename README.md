035a709140abe652b9421bf6b4205200a1da5a85
Eliot Waters <43073961+waterse1080@users.noreply.github.com>
2021-01-16 20:43:43 -0600

    1.0.1 Update
    Logs should be free of null reference errors from non combatant bodies.
    Stat tracking now happens on CharacterMaster instead of CharacterBody.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Space Spinach
-##### by WeeaboTrash
 
 Help your survivors grow up big and strong!
 An uncommon item that increases survivor size by 50% per stack while also slightly increasing health, damage, speed, and jump height.
@@ -7,8 +6,17 @@ An uncommon item that increases survivor size by 50% per stack while also slight
 To install manually, drop the SpaceSpinach.dll file into your BepInEx plugins folder. 
 Commonly found here: C:\Program Files (x86)\Steam\steamapps\common\Risk of Rain 2\BepInEx\plugins
 
+### Changelog:
+## - 1.0.1
+  - Improved performance
+  - Improved consistancy of tracking stats between levels
+
 ### Known Issues:
- - At higher stacks of the item the player's walk cycle speeds up too much for their size.
- - The item shows up as a white box in the command menu rather than properly displaying it's icon.
- - Ranged characters have a harder time hitting enemies near their feet at higher stacks of the item due to camera scaling.
+Major:
+ - Quitting to menu/dying results in the camera position not reseting to normal for that character. For now this can be fixed by restarting the game.
+
+Minor: 
+ - Player walk cycles do not scale well with size, often being much too fast.
+ - The item shows up as a white box in the artifact of command menu.
+ - Ranged characters have a harder time hitting enemies near their feet at higher stacks of the item due to camera position scaling.
 
--- SpaceSpinach/Hooks.cs
+++ SpaceSpinach/Hooks.cs
@@ -9,40 +9,38 @@ namespace SpaceSpinach {
             int spinachCount;
 
             // On character load
-            On.RoR2.CharacterBody.Awake += (orig, self) => {
-                // Call Start
+            On.RoR2.CharacterMaster.Awake += (orig, self) => {
+                // Call Awake
                 orig(self);
-                // Attach the stat-tracking component to each object
+                // Attach stat tracking component to each master object
                 self.gameObject.AddComponent<SpinachStats>();
-                // Set the stats
-                self.gameObject.GetComponent<SpinachStats>().setBaseScale(self.modelLocator.modelTransform.localScale);
-                self.gameObject.GetComponent<SpinachStats>().setCameraPos(self.GetComponent<CameraTargetParams>().cameraParams.standardLocalCameraPos);
-                self.gameObject.GetComponent<SpinachStats>().setBaseHealth(self.baseMaxHealth);
-                self.gameObject.GetComponent<SpinachStats>().setBaseDamage(self.baseDamage);
-                self.gameObject.GetComponent<SpinachStats>().setBaseSpeed(self.baseMoveSpeed);
-                self.gameObject.GetComponent<SpinachStats>().setBaseJump(self.baseJumpPower);
-
-                // Test output
-                /** Chat.AddMessage("ObjectName: " + self.name);
-                Chat.AddMessage("Scale: " + self.gameObject.GetComponent<SpinachStats>().defaultBaseScale);
-                Chat.AddMessage("Health: " + self.gameObject.GetComponent<SpinachStats>().defaultBaseHealth);
-                Chat.AddMessage("Damage: " + self.gameObject.GetComponent<SpinachStats>().defaultBaseDamage);
-                Chat.AddMessage("Speed: " + self.gameObject.GetComponent<SpinachStats>().defualtBaseSpeed); **/                
             };
 
             // Recalculation of numerical stats
             On.RoR2.CharacterBody.RecalculateStats += (orig, self) => {
-                // Get SpaceSpinach #
-                spinachCount = self.inventory.GetItemCount(Assets.SpaceSpinachItemIndex);
+                // Avoiding null reference calls for background birds and level hazards 
+                if (self.inventory != null) {
+                    
+                    // Get SpaceSpinach # if the object has an inventory
+                    spinachCount = self.inventory.GetItemCount(Assets.SpaceSpinachItemIndex);
+                    
+                    // If master stats have not been set, update them. Should happen once per master.
+                    if (self.master.gameObject.GetComponent<SpinachStats>().defaultBaseHealth == 0.0f) {
+                        self.master.gameObject.GetComponent<SpinachStats>().setBaseHealth(self.baseMaxHealth);
+                        self.master.gameObject.GetComponent<SpinachStats>().setBaseDamage(self.baseDamage);
+                        self.master.gameObject.GetComponent<SpinachStats>().setBaseSpeed(self.baseMoveSpeed);
+                        self.master.gameObject.GetComponent<SpinachStats>().setBaseJump(self.baseJumpPower);
+                    }
 
-                // Update HP
-                self.baseMaxHealth = self.gameObject.GetComponent<SpinachStats>().defaultBaseHealth + 10 * spinachCount;
-                // Update Damage
-                self.baseDamage = self.gameObject.GetComponent<SpinachStats>().defaultBaseDamage + 5 * spinachCount;
-                // Update Speed
-                self.baseMoveSpeed = self.gameObject.GetComponent<SpinachStats>().defualtBaseSpeed + spinachCount;
-                // Update Jump Height
-                self.baseJumpPower = self.gameObject.GetComponent<SpinachStats>().defaultBaseJump + 2 * spinachCount;
+                    // Update HP
+                    self.baseMaxHealth = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseHealth + 10 * spinachCount;
+                    // Update Damage
+                    self.baseDamage = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseDamage + 5 * spinachCount;
+                    // Update Speed
+                    self.baseMoveSpeed = self.master.gameObject.GetComponent<SpinachStats>().defualtBaseSpeed + spinachCount;
+                    // Update Jump Height
+                    self.baseJumpPower = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseJump + 2 * spinachCount;
+                }                
 
                 // Call Recalculate stats
                 orig(self);
@@ -50,22 +48,32 @@ namespace SpaceSpinach {
 
             // On entity load or inventory change of entity
             On.RoR2.CharacterBody.OnInventoryChanged += (orig, self) => {
+                
                 // Get the ammount of the item currently on the Character Body
                 spinachCount = self.inventory.GetItemCount(Assets.SpaceSpinachItemIndex);
                 
+                // If master stats have not been set, update them. Should happen once per master.
+                if (self.master.gameObject.GetComponent<SpinachStats>().defaultBaseScale == new Vector3(0.0f, 0.0f, 0.0f)) {
+                    self.master.gameObject.GetComponent<SpinachStats>().setBaseScale(self.modelLocator.modelTransform.localScale);
+                    self.master.gameObject.GetComponent<SpinachStats>().setCameraPos(self.GetComponent<CameraTargetParams>().cameraParams.standardLocalCameraPos);
+                }
+
                 // Update the size of the entity based off of the # of the item held
-                float x = self.gameObject.GetComponent<SpinachStats>().defaultBaseScale.x;
-                float y = self.gameObject.GetComponent<SpinachStats>().defaultBaseScale.y;
-                float z = self.gameObject.GetComponent<SpinachStats>().defaultBaseScale.z;
+                float x = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseScale.x;
+                float y = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseScale.y;
+                float z = self.master.gameObject.GetComponent<SpinachStats>().defaultBaseScale.z;
+                
+                // +50% of base scale per held item
                 self.modelLocator.modelTransform.localScale = new UnityEngine.Vector3(x + spinachCount * x / 2, y + spinachCount * y / 2, z + spinachCount * z / 2);
 
                 // Update camera location
-                Vector3 basePos = self.gameObject.GetComponent<SpinachStats>().defaultCameraPos;
+                // TODO: return to default on exit via menu
+                Vector3 basePos = self.master.gameObject.GetComponent<SpinachStats>().defaultCameraPos;
                 Vector3 spinachPos = new Vector3(0.0f, spinachCount * 1.8f, -spinachCount * 1.8f);
                 self.GetComponent<CameraTargetParams>().cameraParams.standardLocalCameraPos = basePos + spinachPos;
 
                 // Call OnInventoryChanged
-                orig(self);  
+                orig(self);
             };
         }
     }
